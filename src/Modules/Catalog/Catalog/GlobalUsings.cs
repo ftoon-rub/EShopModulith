f7dddac43edf0a57eb5be2a9e1b347e938511585
Ftoon Alrubayea <ftoon-rubayea@hotmail.com>
2025-11-23 21:22:55 +0300

    update 69 video

---
+++ src/Modules/Catalog/Catalog/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using Shared.DDD;
+global using Catalog.Products.Models;
+global using Catalog.Products.Events;
+
